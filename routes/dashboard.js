7ac54a385416a4818c140e3b968c9fe4ec7fe1fc
lanbau <irvinln@gmail.com>
2016-02-02 00:23:45 +0800

    integrate

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var router = express.Router()
 var stormpath = require('express-stormpath')
+var Quandl = require('quandl')
+
 /* GET home page. */
 router.get('/', stormpath.loginRequired, function (req, res, next) {
-  res.render('dashboard', { title: 'Express' })
+  // console.log(req.user)
+  // or more concisely
+  res.render('dashboard')
 })
 
 module.exports = router
