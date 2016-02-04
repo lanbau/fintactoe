9f509b9281a4e8d1c3b0386fb082f44e78f1cf71
lanbau <irvinln@gmail.com>
2016-02-04 10:51:20 +0800

    updates

---
+++ public/js/dragndrop.js
@@ -0,0 +1,15 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    console.log(ev)
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    console.log(ev)
+}
